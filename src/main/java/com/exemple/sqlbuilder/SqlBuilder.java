5c7a6b58c3b0620c1c42309a8de589ea13dbdd94
Antoine V <antoine.verin@etu.unistra.fr>
2022-02-08 23:10:48 -0500

    Implements select queries

--- src/main/java/com/exemple/sqlbuilder/SqlBuilder.java
+++ src/main/java/com/exemple/sqlbuilder/SqlBuilder.java
@@ -2,11 +2,12 @@ package com.exemple.sqlbuilder;
 
 import com.exemple.sqlbuilder.commands.SelectCommand;
 import com.exemple.sqlbuilder.select.SelectQuery;
+import com.exemple.sqlbuilder.select.SelectQueryBuilder;
 
 public class SqlBuilder implements SelectCommand<SelectQuery> {
 
     @Override
     public SelectQuery select(String... columns) {
-        return null;
+        return new SelectQueryBuilder(columns);
     }
 }
---
+++ src/main/java/com/exemple/sqlbuilder/select/SelectQueryBuilder.java
@@ -0,0 +1,60 @@
+package com.exemple.sqlbuilder.select;
+
+public class SelectQueryBuilder implements SelectQuery, FromQuery, LimitQuery, OrderByQuery, WhereQuery, AndQuery, OrQuery {
+
+    private StringBuilder builder = new StringBuilder();
+
+    public SelectQueryBuilder(String... columns) {
+        if(columns.length == 0) throw new IllegalArgumentException("Cannot select anything");
+
+        builder.append("SELECT ").append(columns[0]);
+        for (int i = 1; i < columns.length; i++)
+            builder.append(", ").append(columns[i]);
+    }
+
+    @Override
+    public FromQuery from(String table) {
+        builder.append(" FROM ").append(table);
+        return this;
+    }
+
+    @Override
+    public String build() {
+        return builder.toString();
+    }
+
+    @Override
+    public LimitQuery limit(int limit) {
+        builder.append(" LIMIT ").append(limit);
+        return this;
+    }
+
+    @Override
+    public OrderByQuery orderBy(String column) {
+        return orderBy(column, Order.DESC);
+    }
+
+    @Override
+    public OrderByQuery orderBy(String column, Order order) {
+        builder.append(" ORDER BY ").append(column).append(" ").append(order);
+        return this;
+    }
+
+    @Override
+    public WhereQuery where(String condition) {
+        builder.append(" WHERE ").append(condition);
+        return this;
+    }
+
+    @Override
+    public AndQuery and() {
+        builder.append(" AND");
+        return this;
+    }
+
+    @Override
+    public OrQuery or() {
+        builder.append(" OR");
+        return this;
+    }
+}
