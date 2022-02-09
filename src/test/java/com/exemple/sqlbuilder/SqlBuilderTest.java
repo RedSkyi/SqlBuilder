b3a9b8b645b04b8768650e06f24e1075900fa7d1
Antoine V <antoine.verin@etu.unistra.fr>
2022-02-08 23:11:05 -0500

    Update test query

--- src/test/java/com/exemple/sqlbuilder/SqlBuilderTest.java
+++ src/test/java/com/exemple/sqlbuilder/SqlBuilderTest.java
@@ -1,17 +1,26 @@
 package com.exemple.sqlbuilder;
 
+import com.exemple.sqlbuilder.commands.OrderByCommand;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SqlBuilderTest {
 
+    public static void main(String[] args) {
+        new SqlBuilderTest().test();
+    }
+
     public void test() {
-        new SqlBuilder()
-                .select("nom", "prenom")
-                .from("users")
-                .where("")
-                .and().where("")
-                .orderBy("")
-                .limit(5);
+        System.out.println(
+            new SqlBuilder()
+                    .select("nom", "prenom")
+                    .from("users")
+                    .where("nom = Jean")
+                    .and().where("prenom = Pierre")
+                    .orderBy("nom", OrderByCommand.Order.ASC)
+                    .limit(5)
+                    .build()
+        );
     }
 
 }
