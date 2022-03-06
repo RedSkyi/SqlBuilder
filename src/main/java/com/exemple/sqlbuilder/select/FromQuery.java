7ae4f3bbc9b4acbfd9e6a40c4afa37863cc60552
Antoine V <antoine.verin@etu.unistra.fr>
2022-03-06 10:02:05 -0500

    Add back to line in implementations declaration

--- src/main/java/com/exemple/sqlbuilder/select/FromQuery.java
+++ src/main/java/com/exemple/sqlbuilder/select/FromQuery.java
@@ -8,5 +8,6 @@ import com.exemple.sqlbuilder.commands.WhereCommand;
 public interface FromQuery extends
         WhereCommand<WhereQuery>,
         OrderByCommand<OrderByQuery>,
-        LimitCommand<LimitQuery>, SqlQuery {
+        LimitCommand<LimitQuery>,
+        SqlQuery {
 }
