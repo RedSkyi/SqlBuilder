e9df8d512e5c1eb0e71e3d3d0540c7b6daa35c0b
Antoine V <antoine.verin@etu.unistra.fr>
2022-03-06 10:01:44 -0500

    Add missing SqlQuery implementation of OrderByQuery

--- src/main/java/com/exemple/sqlbuilder/select/OrderByQuery.java
+++ src/main/java/com/exemple/sqlbuilder/select/OrderByQuery.java
@@ -1,6 +1,7 @@
 package com.exemple.sqlbuilder.select;
 
+import com.exemple.sqlbuilder.SqlQuery;
 import com.exemple.sqlbuilder.commands.LimitCommand;
 
-public interface OrderByQuery extends LimitCommand<LimitQuery> {
+public interface OrderByQuery extends LimitCommand<LimitQuery>, SqlQuery {
 }
