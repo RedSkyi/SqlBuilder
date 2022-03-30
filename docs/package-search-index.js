733fe345f16747b34f3650aed65925226b4866f3
RedSkyi <RedSkyi@users.noreply.github.com>
2022-03-30 17:35:27 +0000

    deploy: c4ddb0ea1fedd56cc79725073fd748bea5b31d0d

--- docs/package-search-index.js
+++ docs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.exemple.sqlbuilder"},{"l":"com.exemple.sqlbuilder.commands"},{"l":"com.exemple.sqlbuilder.select"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.exemple.sqlbuilder"},{"l":"com.exemple.sqlbuilder.commands"},{"l":"com.exemple.sqlbuilder.select"},{"l":"com.exemple.sqlbuilder.select.statements"}];updateSearchResults();
